e9b66a846e5772b87f9d40785209f85909ade5e5
Kovács Tamás <tommy1991@vipmail.hu>
2015-10-20 11:03:49 +0200

    Initial commit, to import the project

---
+++ src/main/java/test/the/connection/Main.java
@@ -0,0 +1,12 @@
+package test.the.connection;
+
+/**
+ * Created by joey on 2015.10.20..
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Megy");
+    }
+
+}
