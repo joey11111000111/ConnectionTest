13eff40f2eff1b03491a4df6d76dddc35bbbe065
Kovács Tamás <tommy1991@vipmail.hu>
2015-10-20 11:48:43 +0200

    Improved the code

--- src/main/java/test/the/connection/Main.java
+++ src/main/java/test/the/connection/Main.java
@@ -2,11 +2,27 @@ package test.the.connection;
 
 /**
  * Created by joey on 2015.10.20..
+ * This class has the main method
  */
 public class Main {
 
+    /**
+     * The entry point of the application
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("Megy");
+    }
+
+    /**
+     * Prints a line only made of '-' characters to separate to next and the previous lines
+     * @param count the number of '-' character to use in the separator line
+     */
+    private static void spearatorLine(int count) {
+        StringBuilder sb = new StringBuilder(count);
+        for (int i = 0; i < count; i++)
+            sb.append('-');
+        System.out.println(sb);
     }
 
 }
