71fa88f93df03af6ae56722ecf8e3c4e38574578
Kovács Tamás <tommy1991@vipmail.hu>
2015-10-24 12:04:30 +0200

    started javaFX tutorials

--- src/main/java/test/the/connection/FXEntry.java
+++ src/main/java/test/the/connection/FXEntry.java
@@ -3,9 +3,20 @@ package test.the.connection;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import org.slf4j.Logger;
@@ -24,18 +35,54 @@ public class FXEntry extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        LOG.info("Application is starting");
-        primaryStage.setTitle("First javaFX");
-        Button btn = new Button("Say Hello World");
-        LOG.info("Button is ready in {}", 12);
+        createSignInForm(primaryStage);
+    }
+
+    public void createSignInForm(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahome", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+//        grid.setGridLinesVisible(true);
+
+        Button btn = new Button("Sing in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BASELINE_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                actionTarget.setFill(Color.FIREBRICK);
+                actionTarget.setText("Sign in button pressed (" + (int)(Math.random() * 11) + ')');
             }
         });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+
+        Scene scene = new Scene(grid, 400, 400);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }//class
